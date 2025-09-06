17bce369c8d91ac4366377ecb988d96c7562d794
Colin Eckert <colineckert10@gmail.com>
2025-09-06 07:27:04 -0700

    add firstOrUndefined query util

--- src/lib/db/queries/users.ts
+++ src/lib/db/queries/users.ts
@@ -1,6 +1,7 @@
 import { db } from "..";
 import { users } from "../schema";
 import { eq } from "drizzle-orm";
+import { firstOrUndefined } from "./utils";
 
 export async function createUser(name: string) {
   const [result] = await db.insert(users).values({ name: name }).returning();
@@ -8,17 +9,6 @@ export async function createUser(name: string) {
 }
 
 export async function getUserByName(name: string) {
-  const [user] = await db
-    .select()
-    .from(users)
-    .where(eq(users.name, name))
-    .limit(1);
-  return user || null;
+  const result = await db.select().from(users).where(eq(users.name, name));
+  return firstOrUndefined(result);
 }
-
-// export async function getUserByName(name: string) {
-//   const users = await db.query.users.findMany({
-//     where: (user, { eq }) => eq(user.name, name),
-//   });
-//   return users.length > 0 ? users[0] : null;
-// }
