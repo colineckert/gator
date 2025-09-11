18ec34e206ebb80e0c03eafc1ba22b044a9c8569
Colin Eckert <colineckert10@gmail.com>
2025-09-10 19:57:28 -0700

    Create feedFollows table and createFeedFollow command

---
+++ src/lib/db/queries/follows.ts
@@ -0,0 +1,19 @@
+import { eq } from "drizzle-orm";
+import { db } from "..";
+import { feedFollows, feeds, users } from "../schema";
+
+export async function createFeedFollow(userId: string, feedId: string) {
+  const [newFeedFollow] = await db
+    .insert(feedFollows)
+    .values({ userId, feedId })
+    .returning();
+
+  const [joinedFeeds] = await db
+    .select()
+    .from(feedFollows)
+    .innerJoin(feeds, eq(feedFollows.feedId, feeds.id))
+    .innerJoin(users, eq(feedFollows.userId, users.id))
+    .where(eq(feedFollows.id, newFeedFollow.id));
+
+  return joinedFeeds;
+}
--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, timestamp, unique } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().notNull().defaultRandom(),
@@ -27,3 +27,24 @@ export const feeds = pgTable("feeds", {
 });
 
 export type Feed = typeof feeds.$inferSelect;
+
+export const feedFollows = pgTable(
+  "feed_follows",
+  {
+    id: uuid("id").primaryKey().notNull().defaultRandom(),
+    feedId: uuid("feed_id")
+      .notNull()
+      .references(() => feeds.id, { onDelete: "cascade" }),
+    userId: uuid("user_id")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at")
+      .notNull()
+      .defaultNow()
+      .$onUpdate(() => new Date()),
+  },
+  (table) => ({
+    userFeedUnique: unique("user_feed_unique").on(table.userId, table.feedId),
+  }),
+);
