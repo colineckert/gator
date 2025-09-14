4fcb09b5760a66420a15c129ef9acb16c6a15207
Colin Eckert <colineckert10@gmail.com>
2025-09-13 17:31:17 -0700

    Create posts table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -49,3 +49,19 @@ export const feedFollows = pgTable(
     userFeedUnique: unique("user_feed_unique").on(table.userId, table.feedId),
   }),
 );
+
+export const posts = pgTable("posts", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  feedId: uuid("feed_id")
+    .notNull()
+    .references(() => feeds.id, { onDelete: "cascade" }),
+  title: text("title").notNull(),
+  url: text("url").notNull().unique(),
+  description: text("description"),
+  publishedAt: timestamp("published_at").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
